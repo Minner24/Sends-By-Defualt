fde164a088ff2579b9eef0ed02168b090a098ba8
Minner24 <132414339+Minner24@users.noreply.github.com>
2023-05-22 15:32:24 +0300

    Sends By Defualt Track Template.lua

--- Sends By Defualt Track Template.lua
+++ Sends By Defualt Track Template.lua
@@ -10,7 +10,7 @@
 
 --ENTER NAMES OF FX RECEIVE TRACKS and TEMPLATE SLOT NUMBER BELOW:
 
-UserFXReceiveNames = {"Room", "Med Room", "Big Amb", "Slap", "Echo", "Comp"} --Names of Receive Tracks inside the curly brackets
+UserFXReceiveNames = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"} --Names of Receive Tracks inside the curly brackets
 TemplateNumber = "2" --Put the slot number inside the quotes
 
 ---------------------------------------------------------------
@@ -19,7 +19,7 @@ TemplateNumber = "2" --Put the slot number inside the quotes
 
 -- Disarm record on all tracks
 
-for i=0, reaper.CountTracks(0)-1 do
+for i = 0, reaper.CountTracks(0) - 1 do
   local track = reaper.GetTrack(0, i)
   reaper.SetMediaTrackInfo_Value(track, "I_RECARM", 0)
 end
@@ -34,8 +34,8 @@ end
 
 -- Select the slot for the FX template
 
-command_id = reaper.NamedCommandLookup("_S&M_ADD_TRTEMPLATE"..TemplateNumber, 0)
-reaper.Main_OnCommand(command_id,0)
+command_id = reaper.NamedCommandLookup("_S&M_ADD_TRTEMPLATE" .. TemplateNumber, 0)
+reaper.Main_OnCommand(command_id, 0)
 
 -- Set the selected track to record
 local track = reaper.GetSelectedTrack(0, 0)
@@ -43,47 +43,47 @@ reaper.SetMediaTrackInfo_Value(track, "I_RECARM", 1)
 
 -- Find the tracks with the specified names
 
-local receive_tracks = {}
-for _, name in ipairs(UserFXReceiveNames) do
-  for i = 0, num_tracks - 1 do
-    local cur_track = reaper.GetTrack(0, i)
-    local _, track_name = reaper.GetSetMediaTrackInfo_String(cur_track, "P_NAME", "", false)
+local last_track
+for i = 0, num_tracks - 1 do
+  local cur_track = reaper.GetTrack(0, i)
+  local _, track_name = reaper.GetSetMediaTrackInfo_String(cur_track, "P_NAME", "", false)
+  
+  for _, name in ipairs(UserFXReceiveNames) do
     if track_name == name then
-      receive_track = cur_track
-      break
+      local receive_track = cur_track
+      last_track = cur_track -- Save the last found track
+
+      -- Add a send to the selected track, pointing to the receive track
+      local send = reaper.CreateTrackSend(track, receive_track)
+      -- Set the send properties as desired
+      reaper.SetTrackSendInfo_Value(track, 0, send, "I_DSTCHAN", 2)
+      reaper.SetTrackSendInfo_Value(track, 0, send, "D_VOL", 0)
+      
+      -- Check if the current track is the added track
+      if cur_track ~= track then
+        -- Set the added track to not arm for record
+        reaper.SetMediaTrackInfo_Value(cur_track, "I_RECARM", 0)
+      end
     end
   end
-  if receive_track then
-    -- Add a send to the selected track, pointing to the receive track
-    local send = reaper.CreateTrackSend(track, receive_track)
-    -- Set the send properties as desired
-    reaper.SetTrackSendInfo_Value(track, 0, send, "I_DSTCHAN", 2)
-    reaper.SetTrackSendInfo_Value(track, 0, send, "D_VOL", 0)
-    receive_track = nil
-  end
 end
 
--- Move selected track to bottom of track list
+-- Move selected track below the focused track
 
-function moveTrackToBottom()  -- defines the function
-  reaper.Undo_BeginBlock()  -- begins a block of undoable actions
+function moveTrackBelowFocused()
+  reaper.Undo_BeginBlock()
   
-  -- gets the selected track
-  local sel_track = reaper.GetSelectedTrack(0, 0)
+  local focused_track = reaper.GetSelectedTrack(0, 0)
   
-  -- if a track is selected, then continue
-  if sel_track then
-    -- gets the number of tracks in the project
-    local num_tracks = reaper.CountTracks(0)
+  if focused_track then
+    local focused_track_idx = reaper.GetMediaTrackInfo_Value(focused_track, "IP_TRACKNUMBER")
     
-    -- moves the selected track to the bottom of the track list
-    reaper.ReorderSelectedTracks(num_tracks, 0)
+    if focused_track_idx < num_tracks then
+      local insert_idx = focused_track_idx + 1
+      reaper.InsertTrackAtIndex(insert_idx, true)
+      
+      local new_track = reaper.GetTrack(0, insert_idx)
+      reaper.ReorderSelectedTracks(insert_idx, 0)
+    end
   end
-  
-  -- ends the undo block
-  reaper.Undo_EndBlock('Move selected track to bottom of track list', -1)
 end
-
--- execute the function
-moveTrackToBottom()
-
